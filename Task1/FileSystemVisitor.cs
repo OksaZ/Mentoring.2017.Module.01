54a090ec8c5004d11bbaf91fad5a63d873941363
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-10-11 00:01:27 +0300

    simple iteration implemented

---
+++ Task1/FileSystemVisitor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class FileSystemVisitor
+    {
+        private DirectoryInfo _startDirectory;
+
+        public FileSystemVisitor(string path)
+        {
+            _startDirectory = new DirectoryInfo(path);
+        }
+
+        public IEnumerable<FileSystemInfo> GetFileSystemInfoSequence()
+        {
+            return GetFileSystemInfoSequence(_startDirectory);
+        }
+
+        private IEnumerable<FileSystemInfo> GetFileSystemInfoSequence(DirectoryInfo directory)
+        {
+            foreach (var fileSystemInfo in directory.EnumerateFileSystemInfos())
+            {
+                yield return fileSystemInfo;
+                DirectoryInfo innerDirectory = fileSystemInfo as DirectoryInfo;
+                if (innerDirectory != null)
+                {
+                    foreach (var innerDirectoryFileSystemInfo in GetFileSystemInfoSequence(innerDirectory))
+                    {
+                        yield return innerDirectoryFileSystemInfo;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task1/Mainprg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Mainprg
+    {
+        static void Main(string[] args)
+        {
+            string startPoint = "D:\\Худ. Книги";
+            var visitor = new FileSystemVisitor(startPoint);
+            foreach (var fileSysInfo in visitor.GetFileSystemInfoSequence())
+            {
+                Console.WriteLine(fileSysInfo);
+            }
+        }
+    }
+}
